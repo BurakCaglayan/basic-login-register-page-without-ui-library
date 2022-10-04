61e7dfbd3f6156c8b587ab3c39a4f0a05ae5f6d5
BurakCaglayan <burakkcaglayann@gmail.com>
2022-10-04 23:55:52 +0300

    Create Login component

---
+++ src/components/login/Login.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import FormInput from "../UI/formInput/FormInput";
+
+const Login = () => {
+  const [form, setForm] = useState({
+    name: "",
+    emailAddress: "",
+    password: "",
+  });
+
+  const handleFormChange = (event) => {
+    const { name, value } = event.target;
+
+    const updatedForm = {
+      ...form,
+      [name]: value,
+    };
+
+    setForm(updatedForm);
+  };
+
+  return (
+    <form className="login">
+      <FormInput
+        placeholder="E-Mail Address"
+        type="text"
+        name="emailAddress"
+        value={form.emailAddress}
+        onChange={handleFormChange}
+      />
+      <FormInput
+        placeholder="Password"
+        type="password"
+        name="password"
+        value={form.password}
+        onChange={handleFormChange}
+      />
+    </form>
+  );
+};
+
+export default Login;

6d2142c42e2e84ac7ab5f6f85bfa405b1db75ab5
BurakCaglayan <burakkcaglayann@gmail.com>
2022-10-05 03:15:23 +0300

    Add Label to FormInput component

--- src/components/UI/formInput/FormInput.js
+++ src/components/UI/formInput/FormInput.js
@@ -1,13 +1,17 @@
 const FormInput = ({ name, type = "text", placeholder, value, onChange }) => {
   return (
-    <div className="formInput">
+    <div className="form__group">
       <input
+        id={name}
         placeholder={placeholder}
         type={type}
         name={name}
         value={value}
         onChange={onChange}
       />
+      <label htmlFor={name} className="form__label">
+        {placeholder}
+      </label>
     </div>
   );
 };
